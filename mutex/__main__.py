e0bbc0e36c06f78c948db5fcb7fce9d642f91230
Louis Encinas <enci.louis@gmail.com>
2023-06-04 15:09:07 +0200

    Mecanique mutex avec fichier

---
+++ mutex/__main__.py
@@ -0,0 +1,30 @@
+import os
+
+def clear() -> None:
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def writer() -> None:
+    print("Ongoing treatment (enter to end)")
+    with open("mutex\\file_to_check", "r") as file:
+        input()
+
+def checker() -> bool:
+    try:
+        os.remove(os.path.abspath("mutex\\file_to_check"))
+    except PermissionError:
+        print("Another user already is treating datas")
+        return False
+    else:
+        with open("mutex\\file_to_check", "w"): pass
+        return True
+
+clear()
+
+inp:str = ""
+while inp != "exit":
+
+    print("Start data treatment ('ok' to start, 'exit' to exit) :")
+    inp = input()
+
+    if inp == "ok":
+        if checker(): writer()
