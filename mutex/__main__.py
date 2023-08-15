7a6868b21dd2a9ecf125e0827c4b3ee6751aed90
Louis Encinas <enci.louis@gmail.com>
2023-08-15 14:54:41 +0200

    Correction mutex

--- mutex/__main__.py
+++ mutex/__main__.py
@@ -5,14 +5,14 @@ def clear() -> None:
 
 def writer() -> None:
     print("Ongoing treatment (enter to end)")
-    with open("mutex\\file_to_check", "r") as file:
+    with open("mutex\\file_to_check", "r"):
         input()
 
 def checker() -> bool:
     try:
         os.remove(os.path.abspath("mutex\\file_to_check"))
     except PermissionError:
-        print("Another user already is treating datas")
+        print("Treatment already running in another process")
         return False
     else:
         with open("mutex\\file_to_check", "w"): pass
@@ -23,7 +23,7 @@ clear()
 inp:str = ""
 while inp != "exit":
 
-    print("Start data treatment ('ok' to start, 'exit' to exit) :")
+    print("Start data treatment ('ok' to start, 'exit' to exit)")
     inp = input()
 
     if inp == "ok":
